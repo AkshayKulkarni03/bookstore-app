2a32c451c6d549cd9152dc0c1231ab7d8d00e848
Kulkarni <Akshay.Bapusaheb.Kulkarni@rabobank.nl>
2021-08-10 11:40:39 +0200

    first commit

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+// jest.config.js
+require('jest-preset-angular/ngcc-jest-processor');
+
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+};
